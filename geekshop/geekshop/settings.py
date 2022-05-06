29e009265093851d058ab454cd5198e49eb5bfc8
Alexandra <scribbo@mail.ru>
2022-05-06 14:40:51 +0300

    HW_4
    fixed edit-form

--- geekshop/geekshop/settings.py
+++ geekshop/geekshop/settings.py
@@ -108,7 +108,7 @@ AUTH_USER_MODEL = 'authapp.ShopUser'
 # Internationalization
 # https://docs.djangoproject.com/en/3.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ru'
 
 TIME_ZONE = 'UTC'
 
@@ -129,8 +129,6 @@ STATICFILES_DIRS = [
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
-
-
 
 
 # Default primary key field type
