4dfceb3bf8f2efd60a9f8a6b3dc5244483b3a90e
Alexandra <scribbo@mail.ru>
2022-06-19 16:40:55 +0300

    formset fixed

--- geekshop/ordershop/forms.py
+++ geekshop/ordershop/forms.py
@@ -1,8 +1,10 @@
 from django import forms
-from .models import OrderItem
+from .models import Order, OrderItem
 
 
 class OrderItemForm(forms.ModelForm):
     class Meta:
         model = OrderItem
         exclude = ('order',)
+
+OrderItemFormset = forms.inlineformset_factory(Order, OrderItem, OrderItemForm, extra=2)
--- geekshop/ordershop/views.py
+++ geekshop/ordershop/views.py
@@ -1,7 +1,7 @@
 from .models import Order, OrderItem, OrderItemManager
 from django.forms import inlineformset_factory
 from django.views.generic import ListView, UpdateView
-from ordershop.forms import OrderItemForm
+from ordershop.forms import OrderItemForm, OrderItemFormset
 from django.http.response import HttpResponseRedirect, HttpResponseBadRequest
 from django.shortcuts import render, get_object_or_404
 from django.urls import reverse, reverse_lazy
@@ -66,15 +66,26 @@ class OrderUpdateView(LoginRequiredMixin, TitleMixin, UpdateView):
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        OrderItemFormset = inlineformset_factory(Order, OrderItem, OrderItemForm, extra=2)
-        formset = OrderItemFormset(instance=self.object)
+        formset = kwargs.get('formset', OrderItemFormset(instance=self.object))
         context['orderitems'] = formset
         return context
 
-    @transaction.atomic
-    def form_valid(self, form):
-        OrderItemFormset = inlineformset_factory(Order, OrderItem, OrderItemForm, extra=2)
-        formset = OrderItemFormset(self.request.POST)
-        if formset.is_valid():
-            formset.save()
+    def post(self, request, *args, **kwargs):
+        self.object = self.get_object()
+        form = self.get_form()
+        formset = OrderItemFormset(self.request.POST, instance=self.object)
+        if form.is_valid() and formset.is_valid():
+            return self.form_valid(form, formset)
+        else:
+            return self.form_invalid(form, formset)
+
+    def form_valid(self, form, formset):
+        self.object = self.get_object()
+        formset.save()
         return super().form_valid(form)
+    
+    def form_invalid(self, form, formset):
+        return self.render_to_response(
+            self.get_context_data(form=form, formset=formset)
+        )
+    
